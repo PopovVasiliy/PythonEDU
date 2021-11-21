d2a5ee0ad12ed5129e2cfd628600e0e437b3831c
PopovVasiliy <94162420+PopovVasiliy@users.noreply.github.com>
2021-11-21 19:18:51 +0700

    Add files via upload

--- devicetest.py
+++ devicetest.py
@@ -21,18 +21,23 @@ class DeviceTestCase(unittest.TestCase):
     #     self.assertEqual('line_1', read_line(open_device('/devices/dev4')))
 
     def test_write_line(self):
-        # имитация правильной записи - записываем элемент в коллекцию, затем возвращается последний записанный элемент
-        # и сравнивается по assertEqual с первоначальным значением
-        recorded_element = '35'
-        self.assertEqual(recorded_element, write_line(open_device('/devices/dev1'), recorded_element))
 
-        # имитация неправильной записи - записываем элемент в коллекцию, затем возвращается последний записанный элемент
-        # и сравнивается по assertEqual с 'ошибочным' значением
+        # имитация правильной записи
+        recorded_element = '35'
+        device = open_device('/devices/dev1')
+        write_line(device, recorded_element)
+        self.assertEqual(recorded_element, read_line_without_permission(device))
+
+        # #имитация неправильной записи
         recorded_element = '2314'
         error_recorded_element = '7878756'
-        self.assertEqual(error_recorded_element, write_line(open_device('/devices/dev1'), recorded_element))
+        device = open_device('/devices/dev1')
+        write_line(device, recorded_element)
+        self.assertEqual(error_recorded_element, read_line_without_permission(device))
+
         # проверка на ошибку доступа - ошибка присутствует(тест пройден)
         self.assertRaises(PermissionError, write_line, open_device('/devices/dev0'),'24')
+
         # проверка на ошибку доступа - ошибка отсутствует(тест НЕ пройден)
         self.assertRaises(PermissionError, write_line, open_device('/devices/dev1'),'stroka2')
 
