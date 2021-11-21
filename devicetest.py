cc731f30013185dcfd732a8cc118fdc2e9877229
PopovVasiliy <94162420+PopovVasiliy@users.noreply.github.com>
2021-11-21 13:12:04 +0700

    Add files via upload

--- devicetest.py
+++ devicetest.py
@@ -11,21 +11,20 @@ class DeviceTestCase(unittest.TestCase):
         self.assertIsInstance(open_device('/devices/dev3'), Device)
         self.assertIsInstance(open_device('/devices/dev4'), Device)
 
-        self.assertRaises(IOError, open_device, '/devices/unknown')
+        # self.assertRaises(IOError, open_device, '/devices/unknown')
 
-    # def test_read_line(self):
-    #     self.assertEqual('line_1', read_line(open_device('/devices/dev0')))
-    #     self.assertRaises(PermissionError, read_line, open_device('/devices/dev1'))
-    #     self.assertRaises(IOError, read_line, open_device('/devices/dev2'))
-    #     self.assertEqual('1', read_line(open_device('/devices/dev3')))
-    #     self.assertEqual('line_1', read_line(open_device('/devices/dev4')))
+    def test_read_line(self):
+        self.assertEqual('line_1', read_line(open_device('/devices/dev0')))
+        self.assertRaises(PermissionError, read_line, open_device('/devices/dev1'))
+        self.assertRaises(IOError, read_line, open_device('/devices/dev2'))
+        self.assertEqual('1', read_line(open_device('/devices/dev3')))
+        self.assertEqual('line_1', read_line(open_device('/devices/dev4')))
 
     def test_write_line(self):
-        self.assertRaises(ValueError, write_line(open_device('/devices/dev3'), 35))
-        self.assertRaises(PermissionError, write_line, open_device('/devices/dev1'),'stroka123')
-        self.assertRaises(IOError, write_line, write_line(open_device('/devices/dev0'), 'stroka_test1'))
-        self.assertRaises(PermissionError, write_line, open_device('/devices/dev2'))
-        self.assertRaises(IOError, write_line(open_device('/devices/dev4'), 'stroka_test4'))
+        self.assertEqual('35', write_line(open_device('/devices/dev1'), '35'))
+        self.assertRaises(PermissionError, write_line, open_device('/devices/dev0'),'24')
+        self.assertRaises(PermissionError, write_line, open_device('/devices/dev1'),'stroka2')
+        self.assertEqual('7878756', write_line(open_device('/devices/dev1'), '2314'))
 
 
 if __name__ == '__main__':
